f48fd3a9b00e4aa171ad6554858e84e0cae22548
chinmayitk <chinmayichinnu2001@gmail.com>
2025-02-15 13:46:29 +0530

    Day13

---
+++ wipro_java_project/src/com/wipro/java/designbuilder/builders/CarBuilder.java
@@ -0,0 +1,53 @@
+package com.wipro.java.designbuilder.builders;
+import com.wipro.java.designbuilder.cars.Car;
+import com.wipro.java.designbuilder.cars.CarType;
+import com.wipro.java.designbuilder.components.Engine;
+import com.wipro.java.designbuilder.components.GPSNavigator;
+import com.wipro.java.designbuilder.components.Transmission;
+import com.wipro.java.designbuilder.components.TripComputer;
+
+
+/**
+ * Concrete builders implement steps defined in the common interface.
+ */
+public class CarBuilder implements Builder {
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Car getResult() {
+        return new Car(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
---
+++ wipro_java_project/src/com/wipro/java/designbuilder/cars/CarType.java
@@ -0,0 +1,5 @@
+package com.wipro.java.designbuilder.cars;
+
+public enum CarType {
+    CITY_CAR, SPORTS_CAR, SUV
+}
