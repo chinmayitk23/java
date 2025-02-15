3f363ab5d7672c0f25a8604414c69f1c057b9c41
chinmayitk <chinmayichinnu2001@gmail.com>
2025-02-15 16:07:37 +0530

    Day13

---
+++ wipro_java_project/src/com/wipro/java/collections/hashmap/TreetoHash.java
@@ -0,0 +1,30 @@
+package com.wipro.java.collections.hashmap;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+class TreetoHash {
+  public static void main(String[] args){
+
+    // create a TreeMap of String type
+    TreeMap<String, String> treemap = new TreeMap<>();
+
+    // add mappings to the TreeMap
+    treemap.put("A", "Apple");
+    treemap.put("B", "Ball");
+    treemap.put("C", "Cat");
+    System.out.println("TreeMap: " + treemap);
+
+    // create a HashMap
+    HashMap<String, String> hashmap = new HashMap<>();
+
+    // add mapping to HashMap
+    hashmap.put("Y", "Yak");
+    hashmap.put("Z", "Zebra");
+    System.out.println("Initial HashMap: " + hashmap);
+
+    // add all mappings from TreeMap to HashMap
+    hashmap.putAll(treemap);
+    System.out.println("Updated HashMap: " + hashmap);
+  }
+}
